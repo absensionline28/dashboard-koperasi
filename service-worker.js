9d1ce70ce905516daac9920ec9bb7071fa3891a1
absensionline28 <andiajahbg@gmail.com>
2026-06-20 18:58:44 +0700

    PWA Luxury Gold siap install

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'koperasi-gold-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch file dari cache kalau offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
