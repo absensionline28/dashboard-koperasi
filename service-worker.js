e304b9de87ac8787b07fe58a3736669b09f732ae
absensionline28 <andiajahbg@gmail.com>
2026-06-22 00:24:07 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,25 @@
-// Service worker dimatikan sementara biar app bisa kebuka
-// Nanti kalau udah normal baru diaktifin lagi
+const CACHE_NAME = 'koperasi-gold-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install - simpan file ke cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch - ambil dari cache kalau offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
