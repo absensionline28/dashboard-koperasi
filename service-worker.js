17e61441bba15e3f42cb1606c1c05c8ed102b4bb
absensionline28 <andiajahbg@gmail.com>
2026-06-22 00:43:25 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,10 @@
-const CACHE_NAME = 'koperasi-gold-v3';
+const CACHE_NAME = 'koperasi-gold-v4';
+const BASE_PATH = '/dashboard-koperasi/';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/icon-192.png',
-  '/icon-512.png'
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'style.css', 
+  BASE_PATH + 'script.js',
+  BASE_PATH + 'icon-192.png',
+  BASE_PATH + 'icon-512.png'
 ];
-
-// Install - simpan file ke cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Fetch - ambil dari cache kalau offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
