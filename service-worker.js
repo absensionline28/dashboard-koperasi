104778bdf1810c8f043be0c9228c37f91c4ff3fc
absensionline28 <andiajahbg@gmail.com>
2026-06-22 15:48:29 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,3 +13,22 @@ self.addEventListener('install', event => {
       .then(cache => cache.addAll(urlsToCache))
   );
 });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
