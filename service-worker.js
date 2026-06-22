006c37f2539760f5deade810846cc17a21347bed
absensionline28 <andiajahbg@gmail.com>
2026-06-22 18:19:06 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request).catch(() => caches.match('/index.html'));
-    })
+const CACHE = 'koperasi-v1';
+const URLS = ['/', '/index.html', '/manifest.json', '/icon-192.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
   );
 });
